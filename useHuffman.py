7eca9408386369882f5175749b9fd4fefe8920de
Pranav <pranavmendi@gmail.com>
2021-01-01 23:38:58 +0530

    Added useHuffman.py

---
+++ useHuffman.py
@@ -0,0 +1,11 @@
+from HuffmanCoding import HuffmanCoding
+
+path = "sample.txt"
+
+h = HuffmanCoding(path)
+
+output_path = h.compress()
+print("Compressed file path: " + output_path)
+
+decom_path = h.decompress(output_path)
+print("Decompressed file path: " + decom_path)
